27c3ddcea2906a902c8b5683ebf3bb80ac1499ea
jakob-rzeppa <139046976+jakob-rzeppa@users.noreply.github.com>
2024-03-12 11:10:20 +0100

    Add files via upload

---
+++ object_3d.py
@@ -0,0 +1,57 @@
+import pygame as pg
+from matrix_functions import *
+import json
+
+class Object3D:
+    def __init__(self, render):
+        self.render = render
+
+        with open('objects.json') as JSON:
+            data = json.load(JSON)   
+        self.vertexes = np.array(data['vertexes'])
+        self.faces = np.array(data['faces'])
+
+        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.movement_flag, self.draw_vertexes = False, True
+
+    def draw(self):
+        self.screen_projection()
+        self.movement()
+
+    def movement(self):
+        if self.movement_flag:
+            self.rotate_y(pg.time.get_ticks() % 0.005)
+
+    def screen_projection(self):
+        vertexes = self.vertexes @ self.render.camera.camera_matrix()
+        vertexes = vertexes @ self.render.projection.projection_matrix
+        vertexes /= vertexes[:, -1].reshape(-1, 1)
+        vertexes[(vertexes > 2) | (vertexes < -2)] = 0
+        vertexes = vertexes @ self.render.projection.to_screen_matrix
+        vertexes = vertexes[:, :2]
+
+        for index, color_face in enumerate(self.color_faces):
+            color, face = color_face
+            polygon = vertexes[face]
+            if not np.any((polygon == self.render.H_WIDTH) | (polygon == self.render.H_HEIGHT)):
+                pg.draw.polygon(self.render.screen, color, polygon, 3)
+
+        if self.draw_vertexes:
+            for vertex in vertexes:
+                if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
+
+    def translate(self, pos):
+        self.vertexes = self.vertexes @ translate(pos)
+
+    def scale(self, scale_to):
+        self.vertexes = self.vertexes @ scale(scale_to)
+
+    def rotate_x(self, angle):
+        self.vertexes = self.vertexes @ rotate_x(angle)
+
+    def rotate_y(self, angle):
+        self.vertexes = self.vertexes @ rotate_y(angle)
+
+    def rotate_z(self, angle):
+        self.vertexes = self.vertexes @ rotate_z(angle)
